23e7bdfae1dc967e3998983bd0533921fd7f4de4
carloscalel <49667963+carloscalel@users.noreply.github.com>
2022-09-15 23:35:05 -0600

    subiendo cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 
+import { Product } from './product.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  widthImg = 10;
   name = 'Carlos';
   age = 18;
   img = 'https://th.bing.com/th/id/OIP.12CBhuAELYKyec1x1jcZUgHaEo?pid=ImgDet&rs=1'
@@ -16,6 +19,43 @@ export class AppComponent {
     age: 18,
     avatar: 'https://th.bing.com/th/id/OIP.12CBhuAELYKyec1x1jcZUgHaEo?pid=ImgDet&rs=1'
   }
+
+  names : string[] =  ['nico', 'carlos', 'fernando'];
+  newName =  '';
+
+  products: Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/glasses.jpg'
+    }
+  ]
 
   toggleButton(){
     this.btnDisabled = !this.btnDisabled;
@@ -33,5 +73,14 @@ export class AppComponent {
   changeName(event: Event){
     const element =  event.target as HTMLInputElement;
     this.person.name =  element.value;
+  }
+
+  addName(){
+    this.names.push(this.newName);
+    this.newName = '';
+  }
+
+  deleteName(index: number){
+    this.names.splice(index, 1);
   }
 }
